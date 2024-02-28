14cbdade955986488568fc900b089af98754dff4
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-28 21:44:04 +0100

    middleware

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies
+
   if (!token) return res.status(401).json({ message: 'No autorizado' })
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
